f2fc3af999ce4d32846880add32cee230dc9e3d1
Juliene Witória Viana <119975669+Jojobinea@users.noreply.github.com>
2024-08-11 14:27:32 -0400

    player bullets

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -9,25 +9,27 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Rigidbody2D _rb;
     [SerializeField] private Collider2D _collider;
     [SerializeField] private PlayerInputs _playerInputs;
-    [SerializeField] private GameObject _magicProjectile;
-    [SerializeField] private EquippedProjectileStruct _equippedMagic;
+    [SerializeField] private EquippedProjectileStruct _equippedProjectile;
+    [SerializeField] private LineRenderer _lineRenderer;
 
     // Variables
     [SerializeField] private float _speed;
     private bool _canCastMagic;
+    private bool _isCastingLighting;
 
 
     // Logic 
     private void Start()
     {
-        EventManager.OnPlayerShootEvent += CreateBullet;
+        EventManager.OnPlayerShootEvent += CastMagic;
         EventManager.OnPlayerChangeMagicEvent += ChangePower;
         _canCastMagic = true;
+        _isCastingLighting = false;
     }
 
     private void OnDestroy()
     {
-        EventManager.OnPlayerShootEvent -= CreateBullet;
+        EventManager.OnPlayerShootEvent -= CastMagic;
     }
 
     private void Update()
@@ -40,23 +42,57 @@ public class PlayerController : MonoBehaviour
 
     private void ChangePower(int identifier)
     {
-        _equippedMagic.equippedProjectile = _equippedMagic.magicList[identifier];
+        _equippedProjectile.equippedMagic = identifier;
     }
 
-    private void CreateBullet()
+    private void CastMagic()
+    {
+        if(_equippedProjectile.equippedMagic == 0)
+        {
+            CastFireBall();
+        }
+        else if(_equippedProjectile.equippedMagic == 1)
+        {
+            CastIceShard();
+        }
+    }
+
+    private void CastFireBall()
     {
         if(!_canCastMagic) return;
 
-        Debug.Log("cast magic");
-        GameObject magic = Instantiate(_magicProjectile, transform.position, Quaternion.identity);
-        magic.GetComponent<Projectile>().SetProjectile(_equippedMagic.equippedProjectile);
-        StartCoroutine(MagicCoolDown(magic.GetComponent<Projectile>().GetProjectileStats().coolDownTimer));
+        Debug.Log("cast fire");
+        GameObject magic = Instantiate(_equippedProjectile.magicList[0], transform.position, Quaternion.identity);
+        StartCoroutine(MagicCoolDown(magic.GetComponent<Projectile>().coolDownTimer));
+    }
+
+    private void CastIceShard()
+    {
+        if (!_canCastMagic) return;
+
+        Debug.Log("cast ice");
+
+        int quantity = _equippedProjectile.magicList[1].GetComponent<IceShard>()._shardQuantity;
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePosition - (Vector2)transform.position).normalized;
+
+        for (int i = 0; i < quantity; i++)
+        {
+            float angleOffset = (i - (quantity - 1) / 2f) * 25f;
+            Vector2 newDirection = Quaternion.Euler(0, 0, angleOffset) * direction;
+
+            GameObject magic = Instantiate(_equippedProjectile.magicList[1], transform.position, Quaternion.identity);
+            Rigidbody2D rb = magic.GetComponent<Rigidbody2D>();
+            rb.velocity = newDirection * magic.GetComponent<Projectile>().GetProjectileSpeed();
+        }
+
+        StartCoroutine(MagicCoolDown(_equippedProjectile.magicList[1].GetComponent<Projectile>().coolDownTimer));
     }
 
     private IEnumerator MagicCoolDown(float timer)
     {
         _canCastMagic = false;
-        yield return new WaitForSeconds(_equippedMagic.equippedProjectile.coolDownTimer);
+        yield return new WaitForSeconds(timer);
         _canCastMagic = true;
     }
 }
