d878685ef37a1429d1d3c7ede1d8c6468c0efa27
Daniele Simas <83620485+danisimas@users.noreply.github.com>
2024-08-14 13:51:06 -0400

    update: animation

--- Assets/Scripts/Player/PlayerInputs.cs
+++ Assets/Scripts/Player/PlayerInputs.cs
@@ -6,12 +6,46 @@ using UnityEngine.InputSystem;
 public class PlayerInputs : MonoBehaviour
 {
     private PlayerInputActions _playerInputActions;
+    private Animator _animator;
+    private SpriteRenderer _spriteRenderer;
+
 
     private void Awake()
     {
         _playerInputActions = new PlayerInputActions();
         _playerInputActions.Player.Enable();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
+    private void UpdateMovementAnimation()
+    {
+        Vector2 movementInput = GetMovementVectorValue();
+        
+        if (movementInput.y > 0)
+        {
+            _animator.SetTrigger("MovingForward");
+        }
+        else if (movementInput.y < 0)
+        {
+            _animator.SetTrigger("MovingBack");
+        }
+        else if (movementInput.x > 0)
+        {
+            _animator.SetTrigger("MovingRight");
+            _spriteRenderer.flipX = false;
+        }
+        else if (movementInput.x < 0)
+        {
+            _animator.SetTrigger("MovingLeft");
+            _spriteRenderer.flipX = true;
+        }
+        else
+        {
+            _animator.SetTrigger("Idle");
+        }
+    }
+
 
     private void Update()
     {
